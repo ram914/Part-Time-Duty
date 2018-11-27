e3da1ceb50208fc8648509d6d6638fb235b38fc2
Ram Prasad Gudiwada <25750079+ram914@users.noreply.github.com>
2018-11-27 22:10:32 +0530

    Create ProductionWorker.java

---
+++ ProductionWorker.java
@@ -0,0 +1,20 @@
+public class ProductionWorker extends Employee {
+    private String shift;
+    private double rate;
+    
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+}
